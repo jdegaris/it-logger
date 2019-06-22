ff5f887f3d5dfcd7ae02a9149bd2c79ba40ed6e3
jdegaris <j.a.degaris@gmail.com>
2019-06-22 02:53:08 -0400

    installed Redux - add state management for logs portion of app - CRUD functionality - Search logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Moment from 'react-moment';
 import PropTypes from 'prop-types';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const LogItem = ({ log, deleteLog, setCurrent }) => {
+    const onDelete = () => {
+        deleteLog(log.id);
+        M.toast({ html: 'Log deleted' });
+    }
     return (
         <li className="collection-item">
             <div>
-                <a href='#edit-log-modal' className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>{log.message}</a>
+                <a 
+                    href='#edit-log-modal' 
+                    className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}
+                    onClick={() => setCurrent(log)}
+                >{log.message}
+                </a>
                 <br/>
                 <span className="grey-text">
                     <span className="black-text">ID #{log.id}</span>
                     {' '}last updated by {' '}<span className="black-text">{log.tech}</span>
                     {' '}on {' '}<Moment format='MMMM Do YYYY, h:mm:ss a'>{log.date}</Moment>
                 </span>
-                <a href="#!" className="secondary-content">
+                <a href="#!" className="secondary-content" onClick={onDelete}>
                     <i className="material-icons grey-text">delete</i>
                 </a>
             </div>
@@ -23,6 +36,8 @@ const LogItem = ({ log }) => {
 
 LogItem.propTypes = {
     log: PropTypes.object.isRequired,
+    deleteLog: PropTypes.func.isRequired,
+    setCurrent: PropTypes.func.isRequired,
 }
 
-export default LogItem
+export default connect(null, { deleteLog, setCurrent })(LogItem)
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,26 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-    const [logs, setlogs] = useState([]);
-    const [loading, setLoading] = useState(false);
+import PropTypes from 'prop-types';
+
+
+const Logs = ({ log: { logs, loading }, getLogs }) => {
 
     useEffect(() => {
         getLogs();
         //eslint-disable-next-line
     }, [])
 
-    const getLogs = async () => {
-        setLoading(true);
-        const res = await fetch('/logs');
-        const data = await res.json();
-
-        setlogs(data);
-        setLoading(false);
-    }
-
-    if (loading) {
+    if (loading || logs === null) {
         return <Preloader />
     }
 
@@ -38,4 +32,13 @@ const Logs = () => {
     )
 }
 
-export default Logs
+Logs.propTypes = {
+    log: PropTypes.object.isRequired,
+    getLogs: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+    log: state.log,   
+})
+
+export default connect(mapStateToProps, { getLogs } )(Logs)
